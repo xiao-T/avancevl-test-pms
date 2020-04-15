c380dc013ab1a1b1e7f974c5427784abe65eb541
xiaoT <270199332@qq.com>
2020-04-16 04:07:29 +0800

    update server.js: change prot

--- server.js
+++ server.js
@@ -6,12 +6,12 @@
 const Koa = require('koa')
 const static = require('koa-static')
 const app = new Koa()
-
+const port = process.env.PORT || 8080
 app.use(static('./build'))
 
-app.listen(8080)
+app.listen(port)
 
-console.log('server run on port: 8080')
+console.log(`server run on port: ${port}`)
 // const http = require('http')
 
 // const server = http.createServer((req, res) => {
